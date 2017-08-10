4e35e840f8165353e63c197244829fabc03e9445
vick0823 <f0955798708@gmail.com>
2017-08-10 23:31:40 +0800

    MVC DAY1_HW

---
+++ AccountBook/AccountBook/Controllers/AccountBookController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccountBook.Models;   
+
+namespace AccountBook.Controllers
+{
+    public class AccountBookController : Controller
+    {
+        // GET: AccountBook
+        public ActionResult Index()
+        {
+            List<AccountBookViewModel> data = new List<AccountBookViewModel>();
+            data.Add(new AccountBookViewModel {
+                Type = "收入",
+                Date = "2017-08-09",
+                Money = 1000
+            });
+            data.Add(new AccountBookViewModel
+            {
+                Type = "支出",
+                Date = "2017-08-09",
+                Money = 888
+            });
+            data.Add(new AccountBookViewModel
+            {
+                Type = "收入",
+                Date = "2017-08-10",
+                Money = 66666
+            });
+            return View(data);
+        }
+    }
+}
---
+++ AccountBook/AccountBook/Models/AccountBookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountBook.Models
+{
+    public class AccountBookViewModel
+    {
+        public int RecordSN { get; set; }
+        public string Type { get; set; }
+        public string Date { get; set; }
+        public int Money { get; set; }
+    }
+}
