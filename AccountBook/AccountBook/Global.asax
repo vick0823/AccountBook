77ec71780fff6a0bdff99e2f25d867baf4e8340f
vick0823 <f0955798708@gmail.com>
2017-08-10 22:35:37 +0800

    first commit

---
+++ AccountBook/AccountBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AccountBook.MvcApplication" Language="C#" %>
---
+++ AccountBook/AccountBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AccountBook.Startup))]
+namespace AccountBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
